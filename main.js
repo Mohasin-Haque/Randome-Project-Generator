036e35adc02d128145939f1301d60ed40127c634
Mohasin-Haque <mohasinhaque23121@gmail.com>
2021-10-14 09:22:35 +0530

    Updated new Projects

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 let btn = document.getElementById("btn1");
 let output = document.getElementById("output");
-let quotes = [
-  '"Do your best, God will do the rest ',
-  '"The roots of education is bitter but the fruits are sweet',
-  '"God helps those who helps themselves',
-  '"Tommorow will never come, Do it today',
-  '"If you have a dream and a will to achieve it, nobody can stop you',
-  '"You are best known to you, nobody can know you more then you',
-  '"A person who have never failed in his life has never learned anything new',
-  '"Success comes from failure, if you learn from your mistakes',
-  '"Nobody in this world is honest to you except your parents',
-  '"Never be overconfident.',
+let projects = [
+  ' Smart tutor ',
+  'Make Learning Fun',
+  'App to rate Handwriting',
+  'Solve Unemployment',
+  'Course and university finder',
+  'Apps That Aid College Students',
+  'Healthcare Record/Report Wallet',
+  'Make way for ambulances',
+  'Rural Health care',
+  'Communication between doctors and patients App',
+  'Awareness System',
+  'Better Emergency System',
+  'Ambulance management',
+  'All in one Medical Care App',
+  'App to measure noise level',
+  'Plant care App',
+  'Dedicated App/ website with Product’s environmental impacts',
+  'Better Waste Recycling App',
+  'Iron Man’s AI based Face Recognising System',
+  'Digital Sharable Visiting card',
+  'Automation in Hazardous Jobs',
+  'Inventions to help physically challenged people',
+  'Parking Spot Finder',
+  'Crowd Funding System For Start-Up Owners',
 ];
 btn.addEventListener('click', function(){
-    var randomQuote = quotes[Math.floor(Math.random()* quotes.length)]
-    output.innerHTML = randomQuote;
+    var randomProject = projects[Math.floor(Math.random()* projects.length)]
+    output.innerHTML = randomProject;
 })
